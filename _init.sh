6eb400c572595a59090aa7e5344167d1f2399250
hddyyyb <33851379+hddyyyb@users.noreply.github.com>
2024-12-19 03:01:43 +0800

    Update _init.sh

--- _init.sh
+++ _init.sh
@@ -2,14 +2,10 @@ CUDA_VISIBLE_DEVICES=5
 SAVE_DIR="../lowbit_file/"
 python _init.py \
     --model_name_or_path meta-llama/Llama-2-13b-hf \
-    --token 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 \
+    --token your_token_to_access_llama2_from_HuggingFace_Hub \
     --bits 4 \
     --iter 5 \
     --rank 64 \
     --K 1 \
     --save_dir $SAVE_DIR
-
-
-# 
-
-#     --token your_token_to_access_llama2_from_HuggingFace_Hub \
+ 
